1c680d1d127d9671d1217d5d6da91de213169b21
blindly <@blindly>
2015-10-03 18:09:18 -0400

    test

--- js/feeds.js
+++ js/feeds.js
@@ -1,38 +1,29 @@
     google.load("feeds", "1");
-	
+
     function initialize() {
-	  var feeds = [
-		  			'http://blogs.opera.com/desktop/feed/',
-					  'https://vivaldi.net/blogs/teamblog?format=feed&type=rss'
-	  			]
-				  
-    feeds.forEach(function(entry) {
-      var feed = new google.feeds.Feed( entry);
-      feed.load(function(result) {
-        if (!result.error) {
-          var container = document.getElementById("feed");
-          for (var i = 0; i < result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            var div = document.createElement("div");
-            
-            /*
-            div.appendChild(document.createTextNode(entry.title));
-            container.appendChild(div);
-            */
-            
-        var title = entry.title;
-        var link = entry.link;
+        var feeds = [
+            'http://blogs.opera.com/desktop/feed/',
+            'https://vivaldi.net/blogs/teamblog?format=feed&type=rss',
+            'https://news.ycombinator.com/rss',
+            'http://feeds2.feedburner.com/thenextweb',
+            'http://www.theverge.com/rss/frontpage'
+        ]
 
-        var article_layout = "<div class='article'><a class='title' href='"+ link +"'>"+ title +"</a></div>";
+        feeds.forEach(function(entry) {
+            var feed = new google.feeds.Feed(entry);
+            feed.load(function(result) {
+                if (!result.error) {
+                    for (var i = 0; i < result.feed.entries.length; i++) {
+                        var entry = result.feed.entries[i];
+                        var title = entry.title;
+                        var link = entry.link;
+                        var article_layout = "<div class='article'><a class='title' href='" + link + "'>" + title + "</a></div>";
 
-        $('#news').append( article_layout );
-            
-          }
-        }
-      });
-    });
+                        $('#news').append(article_layout);
+                    }
+                }
+            });
+        });
     }
-	
+
     google.setOnLoadCallback(initialize);
-	
-	
