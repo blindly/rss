a9e8ff9b685f2550bc5cc3c6c3a7c0902db0ffa1
blindly <@blindly>
2015-10-03 19:13:40 -0400

    test

--- js/feeds.js
+++ js/feeds.js
@@ -1,18 +1,16 @@
     google.load("feeds", "1");
 
     function initialize() {
-        var feeds = [
-            'https://news.ycombinator.com/rss',
+        var feeds = ['https://news.ycombinator.com/rss',
             'http://feeds2.feedburner.com/thenextweb',
-            'http://www.theverge.com/rss/frontpage'
-        ]
+            'http://www.theverge.com/rss/frontpage'];
 
-        feeds.forEach(function(entry) {
+        feeds.forEach(function (entry) {
             var feed = new google.feeds.Feed(entry);
-            feed.load(function(result) {
+            feed.load(function (result) {
                 if (!result.error) {
-                    for (var i = 0; i < result.feed.entries.length; i++) {
-                        var entry = result.feed.entries[i];
+                    for (var num = 0; num < result.feed.entries.length; num++) {
+                        var entry = result.feed.entries[num];
                         var title = entry.title;
                         var link = entry.link;
                         var article_layout = "<div class='article'><a class='title' href='" + link + "'>" + title + "</a></div>";
